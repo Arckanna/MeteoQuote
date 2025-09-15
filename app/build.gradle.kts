8e8619cebda127dd3d5186592c7092cc1b73f7df
Arcka <valerie.lecoeur75@gmail.com>
2025-09-15 10:06:22 +0200

    Geolocalisation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.valerie.meteoquote"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "4.3"
+        versionCode = 8
+        versionName = "5"
     }
 
     buildTypes {
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
 
     // Coroutines pour l'appel réseau en arrière-plan
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
